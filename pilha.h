cef64c96b7d1b8185f2c4186cc85f9057fc76cd8
Gabriel-Micael <114935355+Gabriel-Micael@users.noreply.github.com>
2022-10-03 12:03:08 -0300

    Delete pilha.h

--- pilha.h
+++
@@ -1,8 +0,0 @@
-#include "opcoes.cpp"
-
-#ifndef OPCOES_H
-#define OPCOES_H
-    void remover_pilha(int *v, int *top);
-    void inserir_pilha(int *v, int *top, int n);
-    void mostrar_pilha(int *v, int *top);
-#endif
