18e59c30e8374deb2795ff0c4c97b54d158322e9
Gabriel-Micael <114935355+Gabriel-Micael@users.noreply.github.com>
2022-12-07 12:15:17 -0300

    Update arvore.h

--- arvore_binaria_de_busca/arvore.h
+++ arvore_binaria_de_busca/arvore.h
@@ -1,14 +1,10 @@
-#ifdef ARVORE_H
+#include "arvore.cpp"
+#ifndef ARVORE_H
 #define ARVORE_H
-    typedef struct tNode_{
-        int valor;
-        tNode_ * esquerda;
-        tNode_ * direita;
-    }tNode;
     tNode * busca_pre_ordem(tNode * pai, int valor);
-    void * imprime_pre_ordem(tNode * pai);
-    void * imprime_in_ordem(tNode * pai);
-    void * imprime_pos_ordem(tNode * pai);
+    void imprime_pre_ordem(tNode * pai);
+    void imprime_in_ordem(tNode * pai);
+    void imprime_pos_ordem(tNode * pai);
     tNode * inicia_arvore();
     void encerra_arvore();
     void insere(tNode * pai, int valor);
