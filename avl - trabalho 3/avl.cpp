887c3ea1426f07bf0ced967f6dba73b5709eea49
Gabriel-Micael <114935355+Gabriel-Micael@users.noreply.github.com>
2023-02-05 22:43:21 -0300

    Update avl.cpp

--- avl - trabalho 3/avl.cpp
+++ avl - trabalho 3/avl.cpp
@@ -1,27 +1,28 @@
 #include "avl.h"
 
-PONT criarNovoNo(Tveiculo veiculo){
-    PONT novoNo = new NO;
+PONT criarNovoNo(Tveiculo *veiculo){
+    PONT novoNo = (PONT)new NO;
     novoNo->esq = NULL;
     novoNo->dir = NULL;
-    novoNo->veiculo = &veiculo;
+    novoNo->veiculo = veiculo;
     novoNo->bal = 0;
     return novoNo;
 }
 
 int max(int a, int b){
-    return (a>b)? a: b;
+    return (a > b)? a: b;
 }
 
 int altura(PONT p){
-    if (!p)
+    if (!p){
         return -1;
+    }
     return 1 + max(altura(p->esq),altura(p->dir));
 }
 
 int fb(PONT no){
     if(no){
-        return (altura(no->dir) - altura(no->esq));
+        return (altura(no->esq) - altura(no->dir));
     }
     return 0;
 }
@@ -58,109 +59,103 @@ PONT rotacao_esquerda_direita(PONT no){
     return rotacao_simples_a_direita(no);
 }
 
-PONT insere_avl(PONT no, Tveiculo veiculo){
-    cout << "chegou até aqui" << endl;
-    if(no == NULL){
-        cout << "chegou até aqui" << endl;
+PONT insere_avl(PONT no, Tveiculo *veiculo){
+    if(!no){
         return criarNovoNo(veiculo);
-        cout << "chegou até aqui" << endl;
     }else{
-        if(veiculo.placa < no->veiculo->placa)
+        if(veiculo->placa < no->veiculo->placa){
             no -> esq =  insere_avl(no -> esq, veiculo);
-        else if(veiculo.placa > no->veiculo->placa)
+        }
+        else if(veiculo->placa > no->veiculo->placa){
             no -> dir =  insere_avl(no -> dir, veiculo);
-        else
-            return NULL;
+        }else{
+            cout << endl << "ELEMENTO COM PLACA " << veiculo->placa << "JÁ EXISTE!" << endl;
+        }
     }
     no -> bal = max(altura(no -> esq), altura(no -> dir)) + 1;
     no = balancear(no);
+    return no;
 }
 
 PONT remove_avl(PONT no, string placa){
-    if(!no)
+    if(!no){
         return NULL;
-    else
-        if(no -> veiculo -> placa == placa)
-            if(no -> esq == NULL && no -> dir == NULL)
-                return no;
-            else
+    }else{
+        if(no -> veiculo -> placa == placa){
+            if(no -> esq == NULL && no -> dir == NULL){
+                delete(no);
+                return NULL;
+            }
+            else{
                 if(no -> esq != NULL && no -> dir != NULL){
-                    PONT percorre = no -> esq;
-                    while(percorre -> dir != NULL)
+                    PONT percorre = no -> esq, aux = new NO;
+                    while(percorre -> dir != NULL){
                         percorre = percorre -> dir;
+                    }
+                    aux -> veiculo = no -> veiculo;
                     no -> veiculo = percorre -> veiculo;
+                    percorre -> veiculo = aux -> veiculo;
                     no -> esq = remove_avl(no -> esq, placa);
                     return no;
                 }
                 else{
                     PONT percorre;
-                    if(no -> esq != NULL){}
-                       // percorre = no -> esq;
-                    //else
-                        //percorre = no -> esq;
+                    if(no -> esq != NULL){
+                        percorre = no -> esq;
+                    }
+                    else{
+                        percorre = no -> dir;
+                    }
+                    delete(no);
+                    return percorre;
                 }
+            }
+        }
         else{
             if(placa < no -> veiculo -> placa)
                 no -> esq = remove_avl(no -> esq, placa);
             else
-                no -> esq = remove_avl(no -> esq, placa);
+                no -> dir = remove_avl(no -> dir, placa);
         }
+    }
+    no -> bal = max(altura(no -> esq), altura(no -> dir)) + 1;
+    no = balancear(no);
 }
 
 PONT balancear(PONT no){
     int fatorbalanciamento = fb(no);
-    if(fatorbalanciamento < -1 && fb(no -> dir) <= 0)
+    if(fatorbalanciamento < -1 && fb(no -> dir) <= 0){
         no = rotacao_simples_a_esquerda(no);
-    else if(fatorbalanciamento > -1 && fb(no -> esq) >= 0)
+    }else if(fatorbalanciamento > 1 && fb(no -> esq) >= 0){
         no = rotacao_simples_a_direita(no);
-    else if(fatorbalanciamento > -1 && fb(no -> esq) < 0)
+    }else if(fatorbalanciamento > 1 && fb(no -> esq) < 0){
         no = rotacao_esquerda_direita(no);
-    else if(fatorbalanciamento < -1 && fb(no -> dir) > 0)
+    }else if(fatorbalanciamento < -1 && fb(no -> dir) > 0){
         no = rotacao_direita_esquerda(no);
-    return no;
+    }return no;
 }
 
-/* Exibe arvore Em Ordem.         */
-void exibirArvoreEmOrdem(PONT raiz){
-    if (raiz == NULL)
-        return;
-    exibirArvoreEmOrdem(raiz->esq);
-    /*cout << raiz->veiculo->modelo;
-    cout << raiz->veiculo->marca;
-    cout << raiz->veiculo->tipo;
-    cout << raiz->veiculo->ano;
-    cout << raiz->veiculo->km;
-    cout << raiz->veiculo->potencia;
-    cout << raiz->veiculo->combustivel;
-    cout << raiz->veiculo->cambio;
-    cout << raiz->veiculo->direcao;
-    cout << raiz->veiculo->cor;
-    cout << raiz->veiculo->porta;*/
-    cout << "(" << raiz->veiculo->placa;
-    //cout << raiz->veiculo->valor;
-    exibirArvoreEmOrdem(raiz->dir);
-    cout << ")";
+void printAVLTree(PONT node, int level)
+{
+    if (node != NULL)
+    {
+        printAVLTree(node->dir, level + 1);
+        for (int i = 0; i < level; i++)
+            cout << "     ";
+        cout << node->veiculo->placa << endl;
+        printAVLTree(node->esq, level + 1);
+    }
 }
 
 /* Exibe arvore Pre Ordem.         */
 void exibirArvorePreOrdem(PONT raiz){
-    if (raiz == NULL)
+    if (raiz == NULL){
         return;
-    /*cout << raiz->veiculo->modelo;
-    cout << raiz->veiculo->marca;
-    cout << raiz->veiculo->tipo;
-    cout << raiz->veiculo->ano;
-    cout << raiz->veiculo->km;
-    cout << raiz->veiculo->potencia;
-    cout << raiz->veiculo->combustivel;
-    cout << raiz->veiculo->cambio;
-    cout << raiz->veiculo->direcao;
-    cout << raiz->veiculo->cor;
-    cout << raiz->veiculo->porta;*/
-    cout << raiz->veiculo->placa << " ";
-    //cout << raiz->veiculo->valor;
+    }
+    cout << "(" << raiz->veiculo->placa;
     exibirArvorePreOrdem(raiz->esq);
     exibirArvorePreOrdem(raiz->dir);
+    cout << ")";
 }
 
 /* Exibe arvore Pos Ordem         */
@@ -169,20 +164,7 @@ void exibirArvorePosOrdem(PONT raiz){
         return;
     exibirArvorePreOrdem(raiz->esq);
     exibirArvorePreOrdem(raiz->dir);
-    /*cout << raiz->veiculo->modelo;
-    cout << raiz->veiculo->marca;
-    cout << raiz->veiculo->tipo;
-    cout << raiz->veiculo->ano;
-    cout << raiz->veiculo->km;
-    cout << raiz->veiculo->potencia;
-    cout << raiz->veiculo->combustivel;
-    cout << raiz->veiculo->cambio;
-    cout << raiz->veiculo->direcao;
-    cout << raiz->veiculo->cor;
-    cout << raiz->veiculo->porta;*/
-    cout << "(" << raiz->veiculo->placa;
-    //cout << raiz->veiculo->valor;
-    cout << ")";
+    cout << raiz->veiculo->placa << " ";
 }
 
 PONT buscaBinaria(Tveiculo *veiculo, PONT raiz){
