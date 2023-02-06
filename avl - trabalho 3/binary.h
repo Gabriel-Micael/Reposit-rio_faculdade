1014bd290e7a6fbfc0d400792a64a0349dc0af70
Gabriel-Micael <114935355+Gabriel-Micael@users.noreply.github.com>
2023-02-05 22:45:35 -0300

    Update binary.h

--- avl - trabalho 3/binary.h
+++ avl - trabalho 3/binary.h
@@ -1,6 +1,18 @@
 #include "avl.h"
 
+typedef struct no_binary_{
+    no_binary_ *esq;
+    no_binary_ *dir;
+    Tveiculo *veiculo;
+}no_binary;
+
 #ifndef BINARY_H
 #define BINARY_H
+
+no_binary * insert_tree_binary(Tveiculo * veiculo, no_binary * no);
+no_binary * criarNovoNo_binary(Tveiculo *veiculo);
+void printBinaryTree(no_binary *node, int level);
+void destruirArvoreBinary(no_binary * no);
+no_binary * remove_binary(no_binary *no, string placa);
 
 #endif
