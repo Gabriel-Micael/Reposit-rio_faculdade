e3eb872786b6d087658291d49b769625df051a48
Gabriel-Micael <114935355+Gabriel-Micael@users.noreply.github.com>
2023-02-10 02:05:02 -0300

    Update main.cpp

--- avl - trabalho 3/main.cpp
+++ avl - trabalho 3/main.cpp
@@ -26,71 +26,24 @@
 
 using namespace std;
 
-//Função principal
 int main() {
+  // CABEÇALHO DA LISTA ENCADEADA
   cabeca *bd = inicia_lista();
+  // NÓ RAIZ DA ÁRVORE AVL E BINARIA
   PONT treebinary = NULL;
   PONT treeAVL = NULL;
-  ifstream myfile("BD_veiculos.txt");
-  if (myfile.is_open()) {
-    bd->prox = new no;
-    bd->prox->veiculo = new Tveiculo;
-    no *percorre = bd->prox;
-    while (!myfile.eof()) {
-      myfile >> percorre->veiculo->modelo;
-      myfile >> percorre->veiculo->marca;
-      myfile >> percorre->veiculo->tipo;
-      myfile >> percorre->veiculo->ano;
-      myfile >> percorre->veiculo->km;
-      myfile >> percorre->veiculo->potencia;
-      myfile >> percorre->veiculo->combustivel;
-      myfile >> percorre->veiculo->cambio;
-      myfile >> percorre->veiculo->direcao;
-      myfile >> percorre->veiculo->cor;
-      myfile >> percorre->veiculo->porta;
-      myfile >> percorre->veiculo->placa;
-      myfile >> percorre->veiculo->valor;
-      treeAVL = insere_avl(treeAVL, percorre->veiculo);
-      treebinary = insert_tree_binary(treebinary, percorre->veiculo); 
-      if (!myfile.eof()) {
-        percorre->prox = new no;
-        percorre = percorre->prox;
-        percorre -> prox = NULL;
-        percorre->veiculo = new Tveiculo;
-      }
-      bd->tam = bd->tam + 1;
+  // VARIAVEL USADA PARA ESCOLHA DE OPÇÃO NO MENU
+  int opc, opc2;
+  // VARIAVEL USADA PARA RECEBER A PLACA DO VEÍCULO
+  string placa;
+  if((bd -> prox = read_file_listaenc(bd,"BD_veiculos.txt",treeAVL, treebinary))){
+    // CRIANDO ÁRVORES BINÁRIA E AVL PARA RELATÓRIO GERAL
+    for(no* pont = bd -> prox; pont != NULL; pont = pont -> prox){
+      treeAVL = insere_avl(treeAVL, pont->veiculo);
+      treebinary = insert_tree_binary(treebinary, pont -> veiculo);
     }
-    myfile.close();
-    // VARIAVEL USADA PARA ESCOLHA DE OPÇÃO NO MENU
-    int opc, opc2;
-    // VARIAVEL USADA PARA RECEBER A PLACA DO VEÍCULO
-    string placa;
-    // ESTRUTURA PARA GUARDAR AS INFORMAÇÕES DO VEÍCULO NOVO A SER USADO NA SIMULAÇÃO DE ENTRADA
-    Tveiculo *inserir;
-    // LEITURA DE ARQUIVO SIMULANDO ENTRADA DE USUÁRIO
-    //read_file(&inserir, "veiculo_novo.txt");
-    ifstream myfile("veiculo_novo.txt"); 
-    if (myfile.is_open()) {
-      while (!myfile.eof()) {
-        inserir = new Tveiculo;
-        myfile >> inserir->modelo;
-        myfile >> inserir->marca;
-        myfile >> inserir->tipo;
-        myfile >> inserir->ano;
-        myfile >> inserir->km;      //CONSERTAR A FUNÇÂO DE LER ARQUIVO
-        myfile >> inserir->potencia;
-        myfile >> inserir->combustivel;
-        myfile >> inserir->cambio;
-        myfile >> inserir->direcao;
-        myfile >> inserir->cor;
-        myfile >> inserir->porta;
-        myfile >> inserir->placa;
-        myfile >> inserir->valor;
-      }
-      myfile.close();
-    } else {
-      cout << "Unable to open file\n";
-    }
+    // ESTRUTURA A SER USADA NA ENTRADA DE USUÁRIO SIMULADA
+    Tveiculo *inserir = read_file(inserir, "veiculo_novo.txt");
     do {
       // MENU
       cout << endl << "--------------------------------------------------------------"
@@ -109,23 +62,22 @@ int main() {
         salvar(bd);
         break;
       case 1:
-        cout << "DIGITE A PLACA DO VEÍCULO A PROCURAR, NO MODELO AAA0000: ";
-        cin >> placa;
-        if (busca_e_remocao_de_veiculo(bd, placa) == -1) {
-        cout << "ELEMENTO NÃO ENCONTRADO!" << endl;
-        }
+        cout << "[]"; 
         break;
       case 2:
-        cout << "ENTRADA DE USUÁRIO SIMULADA" << endl;
         insercao_veiculo(bd, inserir);
         treeAVL = insere_avl(treeAVL, inserir);
         treebinary = insert_tree_binary(treebinary, inserir);
+        cout << "ENTRADA DE USUÁRIO SIMULADA" << endl;
         break;
       case 3:
         cout << "DIGITE A PLACA DO VEÍCULO A EXCLUIR: ";
         cin >> placa;
-        if(!remove_avl(treeAVL, placa) || !remove_binary(treebinary, placa) || !busca_e_remocao_de_veiculo(bd, placa)){
-          cout << "VALOR NÃO ENCONTRADO!" << endl;
+        if(busca_e_remocao_de_veiculo(bd, placa)){
+          remove_avl(treeAVL, placa);
+          remove_binary(treebinary, placa);
+        }else{
+          cout << "VALOR NÃO ENCONTRADO OU BANCO DE DADOS VAZIO!" << endl;
         }
         break;
       case 4:
@@ -164,7 +116,5 @@ int main() {
     encerra_lista(bd);
     destruirArvore(&treebinary);
     destruirArvore(&treeAVL);
-  } else {
-    cout << "Unable to open file\n";
   }
 }
