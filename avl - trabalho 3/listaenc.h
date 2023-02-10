416baa06b492f33af473d8d732aaa3f5075c59e8
Gabriel-Micael <114935355+Gabriel-Micael@users.noreply.github.com>
2023-02-10 02:06:36 -0300

    Update listaenc.h

--- avl - trabalho 3/listaenc.h
+++ avl - trabalho 3/listaenc.h
@@ -13,12 +13,11 @@ typedef struct cabeca{
 #ifndef LISTAENC_H
 #define LISTAENC_H
 void encerra_lista(cabeca *bd);
-void make_tree_binary(PONT *treebinary, cabeca *bd);
-void make_tree_AVL(PONT *treeAVL, cabeca *bd);
 cabeca * inicia_lista();
 void salvar(cabeca *bd);
 int busca_e_remocao_de_veiculo(cabeca *bd, string placa);
 void insercao_veiculo(cabeca *bd, Tveiculo *veiculo);
 int mostrar(cabeca *bd);
-void read_file(Tveiculo *veiculo, string nome_do_arquivo);
+Tveiculo * read_file(Tveiculo* veiculo, string nome_do_arquivo);
+no * read_file_listaenc(cabeca* bd, string nome_do_arquivo, PONT treeAVL, PONT treebinary);
 #endif
