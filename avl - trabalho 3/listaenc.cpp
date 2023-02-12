246f184f6ddb9e0e23dc49cef0de775cfab9ece5
Gabriel-Micael <114935355+Gabriel-Micael@users.noreply.github.com>
2023-02-11 23:18:42 -0300

    Update listaenc.cpp

--- avl - trabalho 3/listaenc.cpp
+++ avl - trabalho 3/listaenc.cpp
@@ -3,7 +3,12 @@
 #include "binary.h"
 #include <fstream>
 
-//FUNÇÃO PARA FAZER A LEITURA DE UMA LINHA DO ARQUIVO
+//FUNÇÃO PARA FAZER A LEITURA DE UMA LINHA DE ARQUIVO *TXT
+/**
+ * @param veiculo endereço de um estrutura Tveiculo
+ * @param nome_do_arquivo cadeia de caracteres com o nome do arquivo *txt
+ * @return endereço da estrutura Tveiculo que está guardando os dados lidos
+*/
 Tveiculo * read_file(Tveiculo* veiculo, string nome_do_arquivo){
     ifstream myfile(nome_do_arquivo);
     if (myfile.is_open()) {
@@ -32,12 +37,10 @@ Tveiculo * read_file(Tveiculo* veiculo, string nome_do_arquivo){
 /**
  * @param bd endereço de uma estrutura do tipo cabeça (cabeçalho da lista encadeada)
  * @param nome_do_arquivo string com o nome do arquivo *txt a ser lido
- * @param treeAVL endereço de uma estrutura do tipo NO (raiz da árvore avl)
- * @param treebinary endereço de uma estrutura do tipo NO (raiz da árvore binária)
  * @return endereço do cabeçalho da lista encadeada atualizada ou NULL caso não consiga
  * abrir o aquivo *txt
 */
-no * read_file_listaenc(cabeca* bd, string nome_do_arquivo, PONT treeAVL, PONT treebinary){
+no * read_file_listaenc(cabeca* bd, string nome_do_arquivo){
   ifstream myfile(nome_do_arquivo);
   if (myfile.is_open()) {
     bd->prox = new no;
@@ -75,6 +78,11 @@ no * read_file_listaenc(cabeca* bd, string nome_do_arquivo, PONT treeAVL, PONT t
 
 
 // FUNÇÃO PARA IMPRIMIR O VETOR DE STRUCT NA TELA
+/**
+ * @param bd endereço de uma estrutura cabeca
+ * @return 1 caso consiga imprimir os dados da estrutura
+ * @return 0 caso a estrutura esteja "vazia"
+*/
 int mostrar(cabeca *bd) {
   if (bd->prox != NULL) {
     for (no *pont = bd->prox; pont != NULL; pont = pont->prox) {
@@ -99,6 +107,10 @@ int mostrar(cabeca *bd) {
 }
 
 // FUNÇÃO QUE VAI TENTAR INSERIR UM NOVO VEÍCULO AO BANCO DE DADOS
+/**
+ * @param bd endereço de uma estrutura cabeca
+ * @param veiculo endereço de uma estrutura Tveiculo
+*/
 void insercao_veiculo(cabeca *bd, Tveiculo *veiculo) {
   no *novo = new no;
   novo->veiculo = veiculo;
@@ -115,8 +127,17 @@ void insercao_veiculo(cabeca *bd, Tveiculo *veiculo) {
 }
 // FUNÇÃO QUE VAI TENTAR BUSCAR UM ELEMENTO E, SE ACHAR, PERGUNTAR SE DEVE
 // REMOVER
+/**
+ * @param bd endereço de uma estrutura cabeca
+ * @param placa cadeia de caracteres com a identificação da
+ * placa do veículo
+ * @return 0 caso o veículo não seja encontrado ou o banco
+ * de dados esteja vazio
+ * @return 1 caso o veículo seja encontrado mas não removido
+ * @return 2 caso o veículo seja econtrado e removido
+*/
 int busca_e_remocao_de_veiculo(cabeca *bd, string placa) {
-  int opc;
+  int opc = -1;
   if (bd->prox == NULL) {
     return 0;
   }
@@ -129,28 +150,33 @@ int busca_e_remocao_de_veiculo(cabeca *bd, string placa) {
     return 0;
   } else {
     do {
-      cout << "ELEMENTO ENCONTRADO. DESEJA REMOVÊ-LO? ( 1 - SIM, 0 - NÃO) ";
+      cout << "ELEMENTO ENCONTRADO. DESEJA REMOVÊ-LO? ( 1 - SIM, 2 - NÃO) ";
       cin >> opc;
-      if (opc == 0) {
-        return 1;
-      } else if (opc == 1) {
-        if (ant == NULL) {
-          bd->prox = pont->prox;
+      if(verifica_entrada()){
+        if (opc == 2) {
+          return 1;
+        } else if (opc == 1) {
+          if (ant == NULL) {
+            bd->prox = pont->prox;
+          } else {
+            ant->prox = pont->prox;
+          }
+          delete (pont->veiculo);
+          delete (pont);
+          bd->tam = bd->tam - 1;
+          return 2;
         } else {
-          ant->prox = pont->prox;
+          cout << endl << "DIGITE APENAS '1' OU '2'" << endl;
         }
-        delete (pont->veiculo);
-        delete (pont);
-        bd->tam = bd->tam - 1;
-        return 1;
-      } else {
-        cout << endl << "DIGITE APENAS '1' OU '0'" << endl;
       }
-    } while (opc != 0 && opc != 1);
+    } while (opc != 1 && opc != 2);
   }
 }
 
 // FUNÇÃO PARA SALVAR OS DADOS NO ARQUIVO TEXTO
+/**
+ * @param bd endereço de uma estrutura cabeca
+*/
 void salvar(cabeca *bd) {
   int opc, i = 0;
   do {
@@ -162,33 +188,38 @@ void salvar(cabeca *bd) {
          << endl
          << "ESCOLHA UMA OPÇÃO: ";
     cin >> opc;
-    if (opc == 1) {
-      ofstream myfile("BD_veiculos.txt");
-      if (myfile.is_open()) {
-        for (no *pont = bd->prox; pont != NULL; pont = pont->prox, i++) {
-          myfile << pont->veiculo->modelo << " " << pont->veiculo->marca << " "
-                 << pont->veiculo->tipo << " " << pont->veiculo->ano << " "
-                 << pont->veiculo->km << " " << pont->veiculo->potencia << " "
-                 << pont->veiculo->combustivel << " " << pont->veiculo->cambio
-                 << " " << pont->veiculo->direcao << " " << pont->veiculo->cor
-                 << " " << pont->veiculo->porta << " " << pont->veiculo->placa
-                 << " " << pont->veiculo->valor;
-          if(i < bd->tam - 1){
-            myfile << "\n";
+    if(!verifica_entrada()){
+      if (opc == 1) {
+        ofstream myfile("BD_veiculos.txt");
+        if (myfile.is_open()) {
+          for (no *pont = bd->prox; pont != NULL; pont = pont->prox, i++) {
+            myfile << pont->veiculo->modelo << " " << pont->veiculo->marca << " "
+                  << pont->veiculo->tipo << " " << pont->veiculo->ano << " "
+                  << pont->veiculo->km << " " << pont->veiculo->potencia << " "
+                  << pont->veiculo->combustivel << " " << pont->veiculo->cambio
+                  << " " << pont->veiculo->direcao << " " << pont->veiculo->cor
+                  << " " << pont->veiculo->porta << " " << pont->veiculo->placa
+                  << " " << pont->veiculo->valor;
+            if(i < bd->tam - 1){
+              myfile << "\n";
+            }
           }
+          myfile.close();
+        } else {
+          cout << "Unable to open file\n" << endl;
         }
-        myfile.close();
+      } else if (opc == 2) {
       } else {
-        cout << "Unable to open file\n" << endl;
+        cout << endl << "DIGITE APENAS '1' OU '2'!" << endl;
       }
-    } else if (opc == 2) {
-    } else {
-      cout << endl << "DIGITE APENAS '1' OU '2'!" << endl;
     }
   } while (opc != 1 && opc != 2);
 }
 
 //FUNÇÃO QUE INICIA A LISTA ENCADEADA
+/**
+ * @return endereço de uma estrutura cabeca alocada na memória
+*/
 cabeca * inicia_lista(){
   cabeca * novo = new cabeca;
   novo->prox = NULL;
@@ -197,6 +228,9 @@ cabeca * inicia_lista(){
 }
 
 //FUNÇÃO QUE ENCERRA A LISTA ENCADEADA
+/**
+ * @param bd endereço de uma estrutura cabeca
+*/
 void encerra_lista(cabeca *bd) {
   no *ant;
   for (no *pont = bd->prox; pont != NULL;) {
@@ -205,4 +239,17 @@ void encerra_lista(cabeca *bd) {
     delete (ant);
   }
   delete (bd);
+}
+
+/**
+ * @return 0 se o tipo de entrada não for o tipo esperado
+ * @return 1 caso contrário
+*/
+int verifica_entrada(){
+  if(cin.fail()){
+      cin.clear();
+      fflush(stdin);
+      cout << "VALOR INVÁLIDO!" << endl;
+      return 0;
+  }return 1;
 }
